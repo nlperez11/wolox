6e403fea2a76200b58ddc8aee10f250f5cf693fd
Nestor Perez <nestorvzla11@mail.com>
2020-10-24 09:20:14 -0500

    first commit

---
+++ src/main/java/com/wolox/wolox/WoloxChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wolox.wolox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoloxChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoloxChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolox/wolox/WoloxChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolox.wolox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoloxChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
