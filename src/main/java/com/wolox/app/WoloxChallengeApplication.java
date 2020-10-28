52ceab5b1df4a3b967ea4e3f9732367192aea472
Nestor Perez <nestorvzla11@gmail.com>
2020-10-28 11:01:05 -0500

    Sync data from external api on startUp app

--- src/main/java/com/wolox/app/WoloxChallengeApplication.java
+++ src/main/java/com/wolox/app/WoloxChallengeApplication.java
@@ -1,9 +1,13 @@
 package com.wolox.app;
 
+import com.wolox.service.SyncService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -16,7 +20,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class WoloxChallengeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WoloxChallengeApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(WoloxChallengeApplication.class, args);
+		SyncService syncService = context.getBean(SyncService.class);
+		try {
+			syncService.synchronizeUsers();
+			syncService.synchronizeAlbums();
+			syncService.synchronizePhotos();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			SpringApplication.exit(context);
+		}
+
 	}
 
 }
